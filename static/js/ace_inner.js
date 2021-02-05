6b8aedc5dffb4a7da7953acc1f42b276704570dc
John McLear <john@mclear.co.uk>
2021-02-05 12:12:51 +0000

    fix linting error

--- static/js/ace_inner.js
+++ static/js/ace_inner.js
@@ -1,9 +1,10 @@
 'use strict';
+let timer;
 
 $(document).bind('keyup', (e) => {
   if (!parent.parent.pad.myUserInfo.name) {
     clearTimeout(timer);
-    const timer = setTimeout(() => {
+    timer = setTimeout(() => {
       if (!parent.parent.$('#users').hasClass('popup-show')) {
         parent.parent.$('#users').addClass('popup-show');
       }
